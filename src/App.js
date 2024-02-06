3dc2ccf5089f2c3bca88fb22e00092af1df0926f
Sasha Bogolii <sbogolii@siftscience.com>
2024-02-06 16:30:14 +0200

    Added timelines

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function TimeLabel({ hour }) {
 function Hours({ startWork, finishWork }) {
     const startLunch = startWork + 4;
     const finishLunch = startLunch + 1;
-    const hours = arrayRange(1, 48, 1)
+    const hours = arrayRange(1, 72, 1)
         .map(hour => {
             const dayHour = hour % 24;
             let isWorkHour = dayHour >= startWork && dayHour < startLunch
@@ -37,8 +37,65 @@ function Task({ id, estimatedHours, startHour, finishHour }) {
     )
 }
 
-export default function Timeline() {
-    const timelabels = arrayRange(1, 48, 1)
+function Tasks({ csv }) {
+    const assignments = csv.trim().split('\n').map(line => line.trim())
+        .map(line => line.split(","))
+        .map(cells => new Assignment(cells[0], cells[1], cells[2], cells[3]))
+        .map(assignment => {
+            return (
+                <Task id={assignment.taskId} estimatedHours={assignment.estimatedHours}
+                    startHour={assignment.startHour} finishHour={assignment.finishHour}
+                    key={"task-" + assignment.taskId} />
+            )
+        });
+    return (
+        <>
+            {assignments}
+        </>
+    )
+
+}
+
+class Assignment {
+    constructor(taskId, estimatedHours, startHour, finishHour) {
+        this.taskId = taskId;
+        this.estimatedHours = estimatedHours;
+        this.startHour = startHour;
+        this.finishHour = finishHour;
+    }
+}
+
+const e_0_1 = `
+0,3,9,12
+1,7,12,35
+3,4,35,40
+`;
+const e_0_2 = `
+2,1,9,10
+4,7,35,58
+`;
+const e_1_1 = `
+0,3,9,12
+1,7,12,35
+3,4,35,40
+`;
+const e_1_2 = `
+2,1,16,17
+4,7,40,64
+`;
+const e_2_1 = `
+0,3,9,12
+1,7,12,35
+3,4,35,40
+`;
+const e_2_2 = `
+2,1,16,17
+4,7,40,48
+`;
+
+
+function Timeline({ e1, e2, s1, f1, s2, f2 }) {
+    const timelabels = arrayRange(1, 72, 1)
         .map(hour => {
             return <TimeLabel hour={hour} key={hour} />
         });
@@ -48,15 +105,25 @@ export default function Timeline() {
                 {timelabels}
             </div>
             <div className="board-row" key="employee-1">
-                <Hours startWork={9} finishWork={18} />
-                <Task id={0} estimatedHours={8} startHour={9} finishHour={12}/>
+                <Hours startWork={s1} finishWork={f1} />
+                <Tasks csv={e1} />
             </div>
             <div className="board-row" key="employee-2">
-                <Hours startWork={16} finishWork={25} />
-                <Task id={1} estimatedHours={7} startHour={16} finishHour={17} />
+                <Hours startWork={s2} finishWork={f2} />
+                <Tasks csv={e2} />
             </div>
         </div>
     );
+}
+
+export default function Simulation() {
+    return (
+        <>
+            <Timeline e1={e_0_1} e2={e_0_2} s1={9} f1={18} s2={9} f2={18} key="tl0" />
+            <Timeline e1={e_1_1} e2={e_1_2} s1={9} f1={18} s2={16} f2={25} key="tl1" />
+            <Timeline e1={e_2_1} e2={e_2_2} s1={9} f1={18} s2={16} f2={25} key="tl2" />
+        </>
+    )
 }
 
 const arrayRange = (start, stop, step) =>
